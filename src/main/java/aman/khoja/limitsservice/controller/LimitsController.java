3366096a11d88d416a44d1773d2d01094fb60986
Aman Khoja <aak171730@utdallas.edu>
2022-11-22 23:23:54 -0600

    Added configuration class

--- src/main/java/aman/khoja/limitsservice/controller/LimitsController.java
+++ src/main/java/aman/khoja/limitsservice/controller/LimitsController.java
@@ -1,15 +1,21 @@
 package aman.khoja.limitsservice.controller;
 
 import aman.khoja.limitsservice.bean.Limits;
+import aman.khoja.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    public Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits(){
-        return new Limits(1,1000);
+        return new Limits(configuration.getMinimum(),configuration.getMaximum());
+//        return new Limits(1,1000);
     }
 
 
